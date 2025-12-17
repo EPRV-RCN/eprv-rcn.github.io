7537a52fa07b600d0a5e2d7abb9e5adba5c8f45f
jef <jef.dodson@jpl.nasa.gov>
2025-12-17 11:52:08 -0800

    Add comment.

--- js/fig.js
+++ js/fig.js
@@ -1,5 +1,8 @@
-
+// ----------------------------------------------------------------
+// Pagination Max: Max number of figures/items to display per page. 
+// ----------------------------------------------------------------
 const PAG_MAX = 15;
+
 
 // Number of pages surrounding current page to display in
 // pagination navigation
